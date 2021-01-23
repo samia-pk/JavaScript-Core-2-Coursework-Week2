2ed69cf9c3e5fdb73d75aeaab0ce9c5273ca456e
samia-pk <samiaarshad92@gmail.com>
2021-01-23 00:14:24 +0000

    exercises A and B

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,7 +1,15 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
-}
 
+   people.forEach(el => {
+	  let personName = document.createElement('h1');
+	  personName.innerHTML = el.name;
+	  content.append(personName);
+	  let personJob = document.createElement('h2');
+	  personJob.innerHTML = el.job;
+	  content.append(personJob);
+  })
+}
 let people = [
   { name: "Chris", job: "Teacher" },
   { name: "Joanna", job: "Student" },
--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,6 +1,17 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+
+  function shoppingList(arrayOfFood) {
+    
+  let content = document.querySelector('#content');
+  let shoppingUl = document.createElement('ul');
+  content.appendChild(shoppingUl);
+
+  arrayOfFood.forEach((object) => {
+  let shopping1 = document.createElement('li');
+    shopping1.innerHTML = object
+    shoppingUl.appendChild(shopping1);
+});
 }
+
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
