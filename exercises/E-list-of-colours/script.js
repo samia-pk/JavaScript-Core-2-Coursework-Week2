e27e7f67e7023823e14d272b9feec45858cf8e9f
samia-pk <samiaarshad92@gmail.com>
2021-01-26 01:42:02 +0000

    exercise E completed

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,25 @@
 function listOfColours(colours) {
   // Write your code here...
+
+let content = document.getElementById('content');
+  let BoxSelect = document.createElement('select');
+  let paragraph = document.createElement('p');
+
+  for (let color of colours) {
+    let dropColor = document.createElement('option');
+    dropColor.textContent = color;
+
+    BoxSelect.appendChild(dropColor);
+  }
+
+  BoxSelect.add ('change', function () {
+    paragraph.textContent = `You have selected ${BoxSelect.value}!`;
+    paragraph.style.color = `${BoxSelect.value}`;
+  });
+
+  content.appendChild(paragraph);
+  content.appendChild(BoxSelect);
+  
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
