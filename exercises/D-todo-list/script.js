0a700d9e341d11afab85c0faaab396bc881ea546
samia-pk <samiaarshad92@gmail.com>
2021-01-26 01:30:37 +0000

    exercise todo

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,23 @@
 function todoList(todos) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let newUnOderdList = document.createElement("ul");
+  content.appendChild(UnOderdList);
+
+  todos.forEach(function (obj) {
+    let newList = document.createElement("li");
+    newList.innerText = obj["todo"];
+    UnOderdList.appendChild(newList);
+    newList.addEventListener("click", lineThruFunc);
+    function lineThruFunc() {
+      if(newList.style.textDecoration === "line-through"){
+        newList.style.textDecoration = "none";
+      }else{
+            newList.style.textDecoration = "line-through";
+      }
+    }
+  });
+
 }
 
 const todos = [
